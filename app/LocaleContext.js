5ea4d2c212463bfac97c45d2e7e9b5efaa1d6236
slode <rob.m@myplanet.com>
2019-09-19 12:04:17 -0400

    clean up context module

--- app/LocaleContext.js
+++ app/LocaleContext.js
@@ -1,35 +1,6 @@
 import React from 'react'
-
-const DEFAULT_LOCALE = 'en'
+import { DEFAULT_LOCALE } from './Constants'
 
 const LocaleContext = React.createContext(DEFAULT_LOCALE)
 
 export default LocaleContext
-
-// class LocaleProvider extends React.Component {
-//   constructor(props) {
-//     super(props)
-//
-//     this.changeLocale = () => {
-//       this.setState(state => {
-//         const newLocale = state.locale === 'en' ? 'fr' : 'en'
-//         return { locale: newLocale }
-//       })
-//     }
-//
-//     this.state = {
-//       locale: 'en',
-//       changeLocale: this.changeLocale
-//     }
-//   }
-//
-//   render() {
-//     return (
-//       <LocaleContext.Provider value={this.state}>
-//         {this.props.children}
-//       </LocaleContext.Provider>
-//     )
-//   }
-// }
-//
-// export default LocaleProvider
