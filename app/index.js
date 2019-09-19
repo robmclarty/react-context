28b13a9f0b684bea4094d15fb83a670dd2694c3c
slode <rob.m@myplanet.com>
2019-09-19 11:00:07 -0400

    create basic react app base

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div className="my-app">
+    <h1>React, with context</h1>
+
+    This is some content.
+  </div>,
+  document.getElementById('app-container')
+)
